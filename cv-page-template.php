3a0a005034cf1a014b712b3b5295f6db40c087f4
Adria-C <cheung.adria@outlook.com>
2024-03-24 15:42:44 +0800

    Update cv-page-template.php

--- cv-page-template.php
+++ cv-page-template.php
@@ -5,11 +5,11 @@ Template Name: CV
 ?>
 <?php require_once('page_header.php'); ?>
 <div class="container-fluid maincontainer">
-    <div class="container">
+     <div class="container">
         <div class="row">
-            <section class="maintext"> 
-    <article class="excerpts"><!----> <!--we can reuse the css on the old article to restyle the new dynamic posts-->
-
+             <section class="maintext">             
+     <article class="excerpts"><!----> <!--we can reuse the css on the old article to restyle the new dynamic posts-->
+    
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 <div class="post single-page">
@@ -18,23 +18,20 @@ Template Name: CV
         <div class="postcontent">
             <?php /*?><?php the_post_thumbnail(array(150,150), array ('class' => 'alignright')); ?><?php */?>
             <?php the_content(); ?>
-        <?php wp_link_pages(array('before' => '<p><strong>Pages:</strong>', 'after' => '</p>')); ?>
-        </div><!--postcontet-->
-
-     </div><!--post class-->
-
-
-<?php endwhile; ?>
+         <?php wp_link_pages(array('before' => '<p><strong>Pages:</strong>', 'after' => '</p>')); ?>
+         </div><!--postcontet-->
+         </div><!--post class-->
+         <?php endwhile; ?>
 
     <?php else: ?>
-    <div class="post">
-        <p>Sorry, no posts found.</p>
-    </div><!--post-->
-    <?php endif; ?>
-    </article><!---->
+     <div class="post">
+         <p>Sorry, no posts found.</p>
+     </div><!--post-->
+     <?php endif; ?>
+     </article><!---->
 </section>
 
-    </div>
-    </div>
+      </div>
+     </div>
   </div>
-<?php get_footer(); ?> 
+<?php get_footer(); ?>

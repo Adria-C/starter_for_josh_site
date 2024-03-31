7aa458bc39386a4e07591b32aee49b49234c7adf
Adria-C <cheung.adria@outlook.com>
2024-03-31 10:24:29 +0800

    Update header.php
    Fixed the nav bar to work
    - missing the </nav>

--- header.php
+++ header.php
@@ -45,24 +45,26 @@
         <a class="navbar-brand" href="#"> <span class="josh">JOSH</span> <span class="orange">WHITKIN</span></a>
       
         <!-- Toggler/collapsibe Button -->
-       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'your-theme-slug' ); ?>">
-     <span class="navbar-toggler-icon"></span>
-    </button>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'your-theme-slug' ); ?>">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
        <?php 
 
-wp_nav_menu( array(
-  'theme_location'  => 'primary',
-  'depth'           => 2, // 1 = no dropdowns, 2 = with dropdowns.
-  'container'       => 'div',
-  'container_class' => 'collapse navbar-collapse',
-  'container_id'    => 'bs-example-navbar-collapse-1',
-  'menu_class'      => 'navbar-nav ml-auto',
-  'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
-  'walker'          => new WP_Bootstrap_Navwalker(),
-) );
+        wp_nav_menu( array(
+        'theme_location'  => 'primary',
+        'depth'           => 2, // 1 = no dropdowns, 2 = with dropdowns.
+        'container'       => 'div',
+        'container_class' => 'collapse navbar-collapse',
+        'container_id'    => 'bs-example-navbar-collapse-1',
+        'menu_class'      => 'navbar-nav ml-auto',
+        'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+        'walker'          => new WP_Bootstrap_Navwalker(),
+        ) );
 
-?>
+        ?>
+
+    </nav>
 
     <div class="tag">
       <h1>Games Design For The Future</h1>

21dcd78bce7bc0b6573796fa58e5a344c9d8317b
Adria-C <cheung.adria@outlook.com>
2024-05-05 18:04:50 +0800

    Update  script1.js

---  script1.js
+++  script1.js
@@ -1,8 +1,23 @@
 // gsap.from('.navbar-brand', {opacity:0, duration:2, y:-50, ease:"bounce.out"});
 // gsap.from('.tag', {opacity:0, duration:2, x:-50, delay:1.4});
 
+gsap.registerPlugin(ScrollTrigger);
+
 gsap.timeline()
 .from('.navbar', {scale:0, duration:2})
 .from('.navbar-brand', {opacity:0, scale: 0, ease:"back"})
-.from('.tag', {opacity:0, y: 10,  ease:"back"})
-; 
+.from('.tag', {opacity:0, y: 10,  ease:"back"}); 
+
+
+gsap.from(".about-text", {
+        duration:7,
+        y:250,
+        opacity:0,
+        scrollTrigger:{
+            trigger:".aboutbg",
+            markers:true,
+            start:"bottom 90%", ///start at the top of our trigger and a percentage down the viewpost
+            end:"top 30%",
+              scrub:6, //4 seconds of smooting
+    }
+    });
